5dcb95504c3454624651c68f97f9f008203d91e9
Aditya <buddingwebdeveloperind@gmail.com>
2020-09-16 01:19:55 +0530

    almost over with it

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
+
+const daysEl =document.getElementById("days")
+const hoursEl =document.getElementById("hours")
+const minutesEl =document.getElementById("minutes")
+const secondsEl =document.getElementById("seconds")
+
 const newYears = "1 jan 2021";
+
 
 function countdown() {
     const newYearsDate= new Date(newYears);
     const currentDate= new Date();
-    const seconds = (newYearsDate-currentDate)/1000;
-    const days= Math.floor(seconds / 3600 /24);
+    const totalSeconds = (newYearsDate-currentDate)/1000;
+    const days= Math.floor(totalSeconds / 3600 /24);
+    const hours=Math.floor(totalSeconds/3600)% 24;
+    const minutes=Math.floor(totalSeconds/60)%60;
+    const seconds=Math.floor(totalSeconds)%60;
+    console.log(days, hours, minutes, seconds);
 
-    console.log(days);
+    daysEl.innerHTML=days;
+    hoursEl.innerHTML=hours;
+    minutesEl.innerHTML=minutes;
+    secondsEl.innerHTML=seconds;
+
 }
+
 
 
 countdown();
 
-// setInterval(countdown, 1000);
+setInterval(countdown, 1000);
